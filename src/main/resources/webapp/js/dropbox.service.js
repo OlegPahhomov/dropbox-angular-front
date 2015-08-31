04cc2e4912c3eb1e4fc2cc5735287d6e78f7b84d
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-08-31 21:31:52 +0300

    half-done up, doesnt work i think, haven't tried

---
+++ src/main/resources/webapp/js/dropbox.service.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+    angular
+        .module('dropbox')
+        .factory('DropboxService', DropboxService);
+
+    DropboxService.$inject = ['$http', '$upload'];
+    function DropboxService($http, $upload) {
+        var service = {
+            url: serverConfig.SERVER,
+            getFiles: getFiles,
+            getOneFile: getOneFile,
+            deleteFile: deleteFile,
+            addFiles: addFiles,
+        };
+        return service;
+
+        function getFiles() {
+            return $http.get(url + 'files');
+        }
+
+        function getOneFile(id) {
+            return $http.get(url + 'picture/' + id);
+        }
+
+        function deleteFile(id) {
+            return $http.post(url + 'delete/' + id, id);
+        }
+
+        function addFiles(url, data, file) {
+            return $upload
+                .upload({
+                    url: url + '/add',
+                    file: file
+                })
+        }
+    }
+})();
